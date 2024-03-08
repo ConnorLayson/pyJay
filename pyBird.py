97a9de1e985054bc5c343edac39f3490da2530d8
Connor Layson <148878769+ConnorLayson@users.noreply.github.com>
2024-03-08 10:12:21 -0500

    Added animation() and reduce lag
    Reduced lag by adding a display box argument to the map.display()

--- pyBird.py
+++ pyBird.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import json
 
+pygame.font.init()
 print('Thank you for installing the PyBird v.1.0 pygame engine! PyBird was made by Connor Layson.')
 
 class map:
@@ -40,7 +41,7 @@ class map:
                 y += 1
             return rect_list
     
-    def display(self,screen):
+    def display(self,screen,load_box):
         if self.collider:
             print('You can\'t display a collider map.')
         else:
@@ -52,10 +53,12 @@ class map:
                     if tile == '0':
                         pass
                     else:
-                        img_name = self.map_data[0].strip()+tile+'.'+self.ext
-                        img = pygame.image.load(img_name)
-                        img = pygame.transform.scale(img,(img_size,img_size))
-                        screen.blit(img,(((x*img_size)+self.map_x),((y*img_size)+self.map_y)))
+                        hitbox = pygame.Rect((((x*img_size)+self.map_x),((y*img_size)+self.map_y)),(img_size,img_size))
+                        if hitbox.colliderect(load_box):
+                            img_name = self.map_data[0].strip()+tile+'.'+self.ext
+                            img = pygame.image.load(img_name)
+                            img = pygame.transform.scale(img,(img_size,img_size))
+                            screen.blit(img,(((x*img_size)+self.map_x),((y*img_size)+self.map_y)))
                     x += 1
                 y += 1
     
@@ -63,12 +66,63 @@ class map:
         self.map_x += move_x
         self.map_y += move_y
 
-class button:
-    def __init__(self,x,y,text):
+class interactable(object):
+    def __init__(self,file):
+        self.file = file
+        with open(self.file,"r") as f:
+            self.data = json.load(f)
+        
+        self.map = self.data["map_data"]["file"]
+        self.width = self.data["map_data"]["width"]
+        self.height = self.data["map_data"]["height"]
+        self.map_x = 0
+        self.map_y = 0
+        
+        f = open(self.map,'r')
+        data = f.read()
+        f.close()
+        data = data.split('\n')
+        self.game_map = []
+        for row in data:
+            self.game_map.append(list(row))
+        
+    def collide(self,plRect):
+        y = 0
+        self.rect_list = []
+        for row in self.game_map:
+            x = 0
+            for tile in row:
+                if tile == '0':
+                    pass
+                else:
+                    rect = pygame.Rect((x*self.width)+self.map_x,(y*self.height)+self.map_y,self.width,self.height)
+                    rect_data = {
+                        "rect": rect,
+                        "key": tile
+                    }
+                    self.rect_list.append(rect_data)
+                x += 1
+            y += 1
+        for data_point in self.rect_list:
+            if plRect.colliderect(data_point["rect"]):
+                for obj in self.data["objects"]:
+                    if obj["key"] == data_point["key"]:
+                        return obj["function"]
+    
+    def move(self,move_x,move_y):
+        self.map_x += move_x
+        self.map_y += move_y
+    
+    def display(self,screen):
+        for tile in self.rect_list:
+            pygame.draw.rect(screen,(255,255,0),tile["rect"],1)
+
+class button (object):
+    def __init__(self,x,y,text,font=pygame.font.Font(None,50)):
         self.x = x
         self.y = y
         self.text = text
-        self.font = pygame.font.Font(None,50)
+        self.font = font
         self.text_load = self.font.render(text, True, (159, 240, 101))
         self.text_rect = self.text_load.get_rect(center=(self.x,self.y))
         
@@ -108,6 +162,27 @@ class particle:
     def display(self,screen):
         self.screen = screen
         self.star = pygame.draw.circle(self.screen,self.color,(self.x,self.y),self.size)
+
+class animation:
+    def __init__(self,folder,noOfFrames,speed=1,filetype='.png'):
+        self.anim_folder = folder
+        self.frames = noOfFrames
+        self.speed = speed
+        self.counter = speed
+        self.filetype = filetype
+        self.frame = 0
+
+    def slide(self):
+        self.counter -= 1
+        if self.counter == 0:
+            self.counter = self.speed
+            self.frame += 1
+            if self.frame > self.frames-1:
+                self.frame = 0
+        self.image = pygame.image.load(self.anim_folder+str(self.frame)+self.filetype)
+
+    def display(self,screen,x,y):
+        screen.blit(self.image,(x,y))
 
 class announcement:
     def __init__(self,text,img,color,screen):
@@ -156,7 +231,7 @@ class data_management:
     def save(self,data):
         with open(self.name,'w') as save_file:
             json.dump(data,save_file,indent=4)
-        print('[CONSOLE] Saved the data')
+        print('[CONSOLE] Saved the game')
 
     def load(self):
         with open(self.name) as save_file:
